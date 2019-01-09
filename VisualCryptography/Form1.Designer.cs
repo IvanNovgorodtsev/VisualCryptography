1604eb5a6acee8ed548a247e00e8ea0916c70dc6
Ivan Novgorodtsev <ivan.novgorodtsev@gmail.com>
2019-01-09 14:48:18 +0100

    Main loop

--- VisualCryptography/Form1.Designer.cs
+++ VisualCryptography/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(204, 12);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(73, 21);
+            this.openFile.TabIndex = 1;
+            this.openFile.Text = "Open";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- VisualCryptography/Form1.cs
+++ VisualCryptography/Form1.cs
@@ -16,5 +16,52 @@ namespace VisualCryptography
         {
             InitializeComponent();
         }
+
+        Bitmap original;
+        Bitmap output1;
+        Bitmap output2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Users\inovgorodtsev\Desktop\Przechwytywanie.png",
+                Title = "Browse Image Files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                RestoreDirectory = true,
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                original = new Bitmap(textBox1.Text);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            output1 = new Bitmap(original.Width * 2, original.Height * 2);
+            output2 = new Bitmap(original.Width * 2, original.Height * 2);
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                Color color;
+                int p = 0;
+                for (int i = 0; i < original.Width; i++)
+                    for (int j = 0; j < original.Height; j++)
+                    {
+                        color = original.GetPixel(i, j);
+                        if (color.GetBrightness() < 0.01) // If it's black
+                        {
+                            p++;
+                        }
+                        if (color.GetBrightness() > 0.99) // If it's white
+                        {
+                            p++;
+                        }
+                    }
+                MessageBox.Show(p.ToString());
+            }
+        }
     }
 }
